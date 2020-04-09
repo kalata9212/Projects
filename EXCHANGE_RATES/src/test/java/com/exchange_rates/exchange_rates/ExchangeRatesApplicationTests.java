ff7f50fb084efddc491101864af8f455292aa961
kalata9212 <kalata9212@gmail.com>
2020-04-09 22:41:23 +0300

    Rates.

--- EXCHANGE_RATES/src/test/java/com/exchange_rates/exchange_rates/ExchangeRatesApplicationTests.java
+++ EXCHANGE_RATES/src/test/java/com/exchange_rates/exchange_rates/ExchangeRatesApplicationTests.java
@@ -1,10 +1,10 @@
-package com.ecb.ecb;
+package com.exchange_rates.exchange_rates;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EcbApplicationTests {
+class ExchangeRatesApplicationTests {
 
     @Test
     void contextLoads() {
